aa06439c6d42712b51db6036337fe0f27c1ba7c1
migenasynaj <migenasynaj03@gmail.com>
2025-12-02 14:24:02 +0100

    Restore project files after Angular 20 downgrade

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('BookingApp1');
+  // protected readonly title = signal('BookingApp1');
 }
